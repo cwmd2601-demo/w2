70d3f16c09031a1ecee8b42393fac292f9d61cec
cwmd2601-demo <124915406+cwmd2601-demo@users.noreply.github.com>
2023-11-05 19:40:53 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Author: <student name and student number>
+// Program Purposes:description
+
+document.querySelector('#btn').addEventListener('click', () => {
+    document.querySelector('.myClass').classList.add("highlight");
+    document.querySelector('#btn').innerHTML = "Hello";
+    document.querySelector('#btn').style.color = "red";
+})
